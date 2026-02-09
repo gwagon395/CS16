1253f376bb2cfc399dd46284edc6caa441acce47
gwagon395 <maxsroka@ucsb.edu>
2026-02-08 22:32:34 -0800

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,12 @@
+#include <string>
+
+
+// Constants for the search parameters, given as an array
+const int NSEARCHES = 10;   // size of the SEARCHES[] array
+const int SEARCHES[NSEARCHES] = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4};
+
+
+//Constants for the data file that your program is reading in
+const int MAXSIZE = 20;     // amount of integers in the file (you need this to declare your array size)
+const std::string FILENAME = "ArrayFile.txt";    // The file name with the integers
+
